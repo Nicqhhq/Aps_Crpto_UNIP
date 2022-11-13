c20bd778fbf0498370981da8fffdf9faf044ec18
IGALEGOI <egalegoo@gmail.com>
2022-11-13 15:56:00 -0300

    Atualização da tela e remoção do temp
    Adicionado os campos para fazer a decriptação, e definido a posição na tela. Removido o arquivo tempCodeRunnerFile, que foi criado automaticamente.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ tela_principal.geometry("800x600")
 tela_principal.minsize(800, 600)
 tela_principal.maxsize(800, 600)
 
-# FUNÇÕES
+# Funções
 # Mensagem de informação
 def msg_informacao(_texto):
     messagebox.showinfo("Informação", format(_texto, str))
@@ -22,8 +22,9 @@ def msg_erro(_texto):
     messagebox.showerror("Erro!", format(_texto, str))
 
 # Título da página
-label_titulo = Label(text = "Criptografia e Descriptografia", font = "Arial")
+label_titulo = Label(text = "APS - Criptografia e Descriptografia", font = "Arial")
 
+# Criptografia
 # Entrada de dados
 texto_entrada_criptografia = Text(tela_principal, height = 10,
                                   width = 25)
@@ -37,11 +38,38 @@ botao_criptografia = Button(tela_principal, height = 2,
                             width = 20,
                             text = "Criptografar",
                             font = "-weight bold -size 10")
-                            #command = lambda:Take_input())
 
+# Descriptografia
+# Entrada de dados
+texto_entrada_descriptografia = Text(tela_principal, height = 10,
+                                  width = 25)
+
+# Saída de dados
+texto_saida_descriptografia = Text(tela_principal, height = 10,
+                                  width = 25)
+
+# Botão de ação
+botao_descriptografia = Button(tela_principal, height = 2,
+                            width = 20,
+                            text = "Descriptografar",
+                            font = "-weight bold -size 10")
+
+# Execução da Tela 
 label_titulo.pack(ipadx=10, ipady=10)
+label_titulo.place(relx=0.5, rely=0.055, anchor='n')
+
 texto_entrada_criptografia.pack()
+texto_entrada_criptografia.place(relx=0.25, rely=0.2, anchor='n')
 botao_criptografia.pack(padx= 10, pady= 10)
+botao_criptografia.place(relx=0.25, rely=0.5, anchor='n')
 texto_saida_criptografia.pack()
+texto_saida_criptografia.place(relx=0.25, rely=0.6, anchor='n')
+
+texto_entrada_descriptografia.pack()
+texto_entrada_descriptografia.place(relx=0.75, rely=0.2, anchor='n')
+botao_descriptografia.pack(padx= 10, pady= 10)
+botao_descriptografia.place(relx=0.75, rely=0.5, anchor='n')
+texto_saida_descriptografia.pack()
+texto_saida_descriptografia.place(relx=0.75, rely=0.6, anchor='n')
 
 tela_principal.mainloop()
--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-# Tela principal
-tela_principal = Tk()
-tela_principal.title("APS - Criptografia/Descriptografia")
-tela_principal.geometry("800x600+510+153")
-tela_principal.minsize(800, 600)
-tela_principal.maxsize(800, 600)
